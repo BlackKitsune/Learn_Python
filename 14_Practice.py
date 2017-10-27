a3bf4d9c000de7799de792e6a551c25e23d09d77
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-10-27 07:26:59 +0200

    Create 14_Practice.py

---
+++ 14_Practice.py
@@ -0,0 +1,11 @@
+### *** Practice ***
+
+## Determine if a number is even or not
+def is_even(x):
+    if x %2 == 0:
+        return True
+    else:
+        return False
+        
+        
+print is_even(900)
