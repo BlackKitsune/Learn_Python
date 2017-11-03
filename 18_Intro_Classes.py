fad424ab7b0ca717106d957a8332c2ab40aad054
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-11-03 11:46:27 +0100

    Update 18_Intro_Classes.py

--- 18_Intro_Classes.py
+++ 18_Intro_Classes.py
@@ -106,4 +106,209 @@ print zebra.name, zebra.age, zebra.is_alive
 print giraffe.name, giraffe.age, giraffe.is_alive
 print panda.name, panda.age, panda.is_alive
 
+## Methods: Functions inside a class (like __init__())
+# Define the description method/function inside the class
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  # Add your method here!
+  def description(self):
+    print self.name
+    print self.age
 
+hippo = Animal("Theodore", 3)
+hippo.description()
+
+## A class can have any number of member variables
+# They will be available to all memebers of a class
+# Create 2 instances of an animal
+hippo = Animal("Jake", 12)
+cat = Animal("Boots", 3)
+# Ask for one of the variables
+print hippo.is_alive
+# Change that variable
+hippo.is_alive = False
+# Check for what animal changed
+print hippo.is_alive
+print cat.is_alive
+
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  health = "good"
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  # Add your method here!
+  def description(self):
+    print self.name
+    print self.age
+
+hippo = Animal("Theodore", 3)
+hippo.description()
+
+sloth = Animal("Viriato", 6)
+ocelot = Animal("Kurt", 2)
+
+print hippo.health
+print sloth.health
+print ocelot.health
+
+## Example of real class: ShioppingCart
+class ShoppingCart(object):
+  """Creates shopping cart objects
+  for users of our fine website."""
+  items_in_cart = {}
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+
+  def add_item(self, product, price):
+    """Add product to the cart."""
+    if not product in self.items_in_cart:
+      self.items_in_cart[product] = price
+      print product + " added."
+    else:
+      print product + " is already in the cart."
+
+  def remove_item(self, product):
+    """Remove product from the cart."""
+    if product in self.items_in_cart:
+      del self.items_in_cart[product]
+      print product + " removed."
+    else:
+      print product + " is not in the cart."
+
+my_cart = ShoppingCart("Geralt")
+my_cart.add_item("Witcher Sword", 200)
+
+## Inheritance: Process by which one class takes on the attributes
+# and methods of another to express a relationship
+# Ex: A Panda is a bear so it will inherit from bear class
+
+class Customer(object):
+  """Produces objects that represent customers."""
+  def __init__(self, customer_id):
+    self.customer_id = customer_id
+
+  def display_cart(self):
+    print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+  """For customers of the repeat variety."""
+  def display_order_history(self):
+    print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+
+## Inheritance syntax: 
+# class DerivedClass(BaseClass):
+  # code goes here
+
+class Shape(object):
+  """Makes shapes!"""
+  def __init__(self, number_of_sides):
+    self.number_of_sides = number_of_sides
+
+# Add your Triangle class below!
+class Triangle(Shape):
+  def __init__(self, side1, side2, side3):
+    self. side1 = side1
+    self. side2 = side2
+    self. side3 = side3
+
+## Override from the parents class
+class Employee(object):
+  def __init__(self, name):
+    self.name = name
+  def greet(self, other):
+    print "Hello, %s" % other.name
+
+class CEO(Employee):
+  def greet(self, other):
+    print "Get back to work, %s!" % other.name
+
+ceo = CEO("Emily")
+emp = Employee("Steve")
+emp.greet(ceo)
+# Hello, Emily
+ceo.greet(emp)
+# Get back to work, Steve!
+
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+  
+# This class changes the wage of a part time employee
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
+
+# Access to the superclass method (super call)
+# m() method derived from the base class
+# class Derived(Base):
+  # def m(self):
+    # return super(Derived, self).m()
+
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
+  
+  def full_time_wage(self, hours):
+    return super(PartTimeEmployee, self).calculate_wage(hours)
+
+milton = PartTimeEmployee('Milton')
+print milton.full_time_wage(10)
+
+## Recap:
+class Triangle(object):
+  # Member variables
+  number_of_sides = 3
+  
+  # Initialize the class
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1
+    self.angle2 = angle2
+    self.angle3 = angle3
+  
+  # Method for checking sum(angles) = 180
+  def check_angles(self):
+    if (self.angle1 + self.angle2 + self.angle3 == 180):
+      return True
+    else:
+      return False
+    
+# Instance Triangle class
+my_triangle = Triangle(90,30,60)
+print my_triangle.number_of_sides
+print my_triangle.check_angles()
+
+# Inheritance
+class Equilateral(Triangle):
+  # Member variable
+  angle = 60
+  def __init__(self):
+    self.angle1 = self.angle
+    self.angle2 = self.angle
+    self.angle3 = self.angle
